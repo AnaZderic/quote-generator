e5575862d3763f3f5a9d7002fcf3a22afd9e7505
Ana Žderić <zderic.ana20@gmail.com>
2023-06-13 23:41:15 +0200

    Add Tweet Quote and New Quote functionality and Loader

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
 
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+// Show Loading
+function loading() {
+    loader.hidden = false; 
+    quoteContainer.hidden = true; 
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false; 
+    loader.hidden = true; 
+}
 
 // Show New Quote
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array 
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //Check if Author field is blank and replace it with 'Unknown'
@@ -23,11 +38,14 @@ function newQuote() {
        quoteText.classList.remove('long-quote');
     }
 
+    // Set Quote, Hide Loader 
     quoteText.textContent = quote.text
+    complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl); 
@@ -37,6 +55,16 @@ async function getQuotes() {
         // Catch Error Here
     }
 }
+
+//Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote); 
 
 // On Load 
 getQuotes();
